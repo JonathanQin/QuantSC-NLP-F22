2a526f8c5b59a59a71be33f38b4d77c5ccd3eeec
Logan Norman <logannormanss@gmail.com>
2022-10-23 20:49:34 -0700

    finished web scraper

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import time
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,6 +10,7 @@ class WaPoScraper():
     def __init__(self):
         self.driver = webdriver.Chrome() # add arg of path to chromedriver
         self.driver.get('https://www.washingtonpost.com/tech-policy/?itid=nb_technology_tech-policy');
+        self.output = None
         time.sleep(2) # Let the user actually see something!
 
     # close the chrome instance
@@ -17,19 +19,42 @@ class WaPoScraper():
 
     # get article links on page
     def getLinks(self):
-        headings = self.driver.find_elements(By.TAG_NAME, 'h3')
-        stories = self.driver.find_elements(By.CLASS_NAME, 'story-headline')
+        print("loading articles...")
         objs = self.driver.find_elements(By.CSS_SELECTOR,
                                         "div[data-feature-id='homepage/story']")
+
+        headlines = []
+        timestamps = []
         for o in objs:
-            headline = o.find_element(By.TAG_NAME, 'h3').text
-            timestamp = o.find_elements(By.TAG_NAME, 'span')[-1].text
+            h = o.find_element(By.TAG_NAME, 'h3').text
+            t = o.find_elements(By.TAG_NAME, 'span')[-1].text
+            
+            headlines.append(h)
+            timestamps.append(t)
 
-            print(headline, timestamp)
+        print("successfully got all article headlines and timestamps")
+        self.export(headlines, timestamps)
 
+    def export(self, headlines, timestamps):
+        industry = ['Tech' for _ in range(len(headlines))]
+
+        d = {}
+        d['Headlines'] = headlines
+        d['Timestamps'] = timestamps
+        d['Industry'] = industry
+
+        df = pd.DataFrame(d)
+        df.to_csv('wapo.csv')
+        print("finished exporting dataset")
+
+    def getOutput(self):
+        return self.output
+
+    # keep clicking the load more articles button
     def loadArticles(self):
         parent = self.driver.find_element(By.XPATH, '//*[@id="__next"]/div[3]/div/main/article')
-        while True:
+        for i in range(50):
+            print('clicking the load button...', i)
             try:
                 loadBtn = parent.find_element(By.TAG_NAME, 'button')
                 loadBtn.click()
@@ -44,9 +69,10 @@ class WaPoScraper():
 def main():
     scraper = WaPoScraper()
     try:
-        scraper.loadArticles()
-        scraper.getLinks()
+        scraper.loadArticles() # first, get all articles on screen
+        scraper.getLinks() # parse the articles on screen
         scraper.quit()
+
     except Exception as e:
         print('failed')
         print(e)
